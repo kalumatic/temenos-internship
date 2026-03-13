aeb029712c715693d3df151e20ae73dc0e45cb3c
Luka Matic <luka.matic@mera.com>
2026-03-13 14:12:55 +0100

    Initial commit

---
+++ src/main/java/com/temenos/temenosinternship/TemenosInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.temenos.temenosinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemenosInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemenosInternshipApplication.class, args);
+    }
+
+}
