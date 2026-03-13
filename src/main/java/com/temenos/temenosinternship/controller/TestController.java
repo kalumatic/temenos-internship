4bb177a8df7f351d74f5335c1eaa83d5ab4540f8
Luka Matic <luka.matic@mera.com>
2026-03-13 15:37:27 +0100

    Implement custom logging feature

--- src/main/java/com/temenos/temenosinternship/controller/TestController.java
+++ src/main/java/com/temenos/temenosinternship/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.temenos.temenosinternship.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,11 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class TestController {
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/test")
     public Mono<ResponseEntity<String>> test() {
+        log.debug("/test called");
         return Mono.just(ResponseEntity.ok("test ok"));
     }
 }
